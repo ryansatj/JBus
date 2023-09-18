2335e3a394f707add899bb3c7eaceb557be253b2
Ryan <ryansaftje123@gmail.com>
2023-09-18 21:56:31 +0700

    PT02 - revisi Post Test 2

--- RyanSafaTjendanaJBusAF/Price.java
+++ RyanSafaTjendanaJBusAF/Price.java
@@ -3,30 +3,31 @@ package RyanSafaTjendanaJBusAF;
 public class Price
 {
     public double price;
-    public int discount;
+    //public int discount;
     public double rebate;
     
     public Price(double price)
     {
         this.price = price;
-        this.discount = 0;
+        //this.discount = 0;
         this.rebate = 0;
     }
     
-    public Price(double price, int discount)
+    /**public Price(double price, int discount)
     {
         this.price = price;
         this.discount = discount;
         this.rebate = 0;
-    }
+    }**/
     
     public Price(double price, double rebate)
     {
         this.price = price;
         this.rebate = rebate;
-        this.discount = 0;
+        //this.discount = 0;
     }
     
+    /**
     private double getDiscountedPrice()
     {
         if (this.discount >= 100)
@@ -45,6 +46,6 @@ public class Price
          }
          
          return this.price - this.rebate;
-    }
+    }**/
 
 }
